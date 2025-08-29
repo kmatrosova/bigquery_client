21468d7df629318842b7bb44d36f1f73a1856a97
Amine Akkouche <amine@MacBook-Air-de-Amine.local>
2025-08-29 12:42:21 +0200

    Name change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="bigquery-client",
+    name="bigquery_client",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
